66cbeef634958a14c63e7a6a19ad7efb20301fc1
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-12-20 22:54:25 +0100

    Completed app layout, added functions to buttons

--- ToDoList-app/src/ToDoList.jsx
+++ ToDoList-app/src/ToDoList.jsx
@@ -1,3 +1,44 @@
 import { useState } from "react";
 
+function ToDoList() {
+  const [tasks, setTasks] = useState(["Take shower", "Read book", "Excercise"]);
+  const [newTask, setNewTask] = useState("");
+
+  function addTaks() {}
+
+  function removeTask(index) {}
+
+  function moveUp(index) {}
+
+  function moveDown(index) {}
+
+  return (
+    <>
+      <div className="todolist-container">
+        <h1>ToDoList app</h1>
+
+        <ol>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span className="text">{task}</span>
+              <button className="remove-btn" onClick={() => removeTask(index)}>
+                Remove
+              </button>
+              <button className="move-btn" onClick={() => moveUp(index)}>
+                ☝️
+              </button>
+              <button className="move-btn" onClick={() => moveDown(index)}>
+                👇
+              </button>
+            </li>
+          ))}
+        </ol>
+
+        <input type="text" className="taskInput" value={newTask} />
+        <button onClick={addTaks}>Add task</button>
+      </div>
+    </>
+  );
+}
+
 export default ToDoList;
